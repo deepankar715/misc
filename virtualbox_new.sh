646de91702cef775ee84990e615f2761c5620eee
deepankar715 <13715891+deepankar715@users.noreply.github.com>
2019-11-07 13:58:32 +0530

    Update virtualbox_new.sh

--- virtualbox_new.sh
+++ virtualbox_new.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# script for installing virtualbox on centos
 # rport errors and improvements and bugs and everything else to 'dep1943008@sicsr.ac.in'
+# run as sudo or root
 
 yum install -y kernel-devel kernel-headers gcc make perl wget dnf 'dnf-command(config-manager)'
 rpm --import https://www.virtualbox.org/download/oracle_vbox.asc
 dnf config-manager --add-repo=https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
 dnf install -y VirtualBox-6.0
 virtualbox
-
