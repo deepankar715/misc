092837c3363083ee1ddf6b346d0c8b81c9666307
deepankar715 <13715891+deepankar715@users.noreply.github.com>
2019-12-04 12:33:17 +0530

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -4,7 +4,7 @@
 # rport errors and improvements and bugs and everything else to 'dep1943008@sicsr.ac.in'
 # run as sudo or root
 
-yum install -y kernel-devel kernel-headers gcc make perl wget dnf 'dnf-command(config-manager)'
+yum install -y kernel-devel kernel-headers gcc make perl wget dnf 'dnf-command(config-manager)' elfutils-libelf-devel
 rpm --import https://www.virtualbox.org/download/oracle_vbox.asc
 dnf config-manager --add-repo=https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo
 dnf install -y VirtualBox-6.0
