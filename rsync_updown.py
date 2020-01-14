7008173588b26bc6d7ed323ca41b103edf002e94
deepankar715 <13715891+deepankar715@users.noreply.github.com>
2020-01-14 10:26:35 +0530

    Update rsync_updown.py

--- rsync_updown.py
+++ rsync_updown.py
@@ -14,35 +14,29 @@ def upload(ip):
 def download(ip):
     os.system(f"rsync -azv --no-perms --no-times --size-only /run/user/1000/gvfs/sftp:host={ip},port=2222/data/data/com.arachnoid.sshelper/home/SDCard/rsync /home/$USER/rsync/ ")
 
-def main():
-    if sys.argv[1] == '--help':
-        print("""
+def help():
+    print("""
         Usage: ./rsync_updown.py [mode] [ip address]
         Modes:
         -d : for downloading
         -u : for uploading
             """)
+    
+def main():
+    if sys.argv[1] == '--help':
+        help()
     elif len(sys.argv) != 3:
         print("see usage --help")
     else:
         if sys.argv[1] == '-d':
-            mode = "download"
-        elif sys.argv[1] == '-u':
-            mode = "upload"
-        elif sys.argv[1] == '--help':
-            print("""
-            Usage: ./script.py [mode] [ip address]
-            Modes:
-            -d : for downloading
-            -u : for uploading
-                """)
-        else:
-            print("see usage --help")
-
-        if mode == "upload":
-            upload(sys.argv[2])
-        if mode == "download":
             download(sys.argv[2])
+        elif sys.argv[1] == '-u':
+            upload(sys.argv[2])
+        elif sys.argv[1] == '--help':
+            help()
+        else:
+            help()
+
 
 if __name__ == '__main__':
     main()
